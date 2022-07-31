c27c69fe332654d2eb201e5ab82f4e928b3667a6
Sayalic2831 <98662059+Sayalic2831@users.noreply.github.com>
2022-07-31 18:44:56 +0530

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,57 @@
+package Task_2;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI {
+    public GUI() {
+        Welcome welcomePage = new Welcome();
+        Game game = new Game();
+
+        welcomePage.playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                welcomePage.frame.setVisible(false);
+                game.frame.setVisible(true);
+            }
+        });
+
+        game.submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int number = game.getGuessedNumber();
+                if(number != -1) {
+                    game.remaining_attempts--;
+
+                    if(game.randomNumber == number) {
+                        game.hasWon = true;
+                        Over gameOver = new Over(game);
+                        game.frame.setVisible(false);
+                        gameOver.frame.setVisible(true);
+                    } else {
+                        if(number > game.randomNumber) {
+                            game.status.setText("Incorrect, guessed value smaller than " + number);
+                            game.guessedNumber.setText(" Enter your guess ");
+                        } else {
+                            game.status.setText("Incorrect, guessed value greater than " + number);
+                            game.guessedNumber.setText(" Enter your guess ");
+                        }
+                    }
+
+                    if(game.remaining_attempts == 0) {
+                        Over gameOver = new Over(game);
+                        game.frame.setVisible(false);
+                        gameOver.frame.setVisible(true);
+                    } else {
+                        game.attempts.setText("Number of Attempts remaining: " + game.remaining_attempts);
+                    }
+                }
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
---
+++ Welcome.java
@@ -0,0 +1,35 @@
+package Task_2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Welcome {
+
+    JFrame frame;
+    JButton playButton;
+
+    public Welcome() {
+        frame = new JFrame();
+
+        JLabel title = new JLabel("Guess The Number");
+        title.setHorizontalAlignment(JLabel.CENTER);
+        frame.add(title);
+
+        JPanel jPanel = new JPanel();
+        frame.add(jPanel);
+
+        playButton = new JButton("Play");
+        playButton.setHorizontalAlignment(JButton.CENTER);
+        playButton.setSize(200, 150);
+        jPanel.add(playButton);
+
+        frame.setTitle("Guess The Number");
+        frame.setResizable(false);
+        frame.setSize(300, 250);
+        frame.setLayout(new GridLayout(2, 1));
+        frame.setVisible(true);
+    }
+
+}
